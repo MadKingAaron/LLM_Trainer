e908fa73bbe112eecdfe20e51ee86085e2e20804
Aaron Jimenez <aaron_jimenez@ucsb.edu>
2023-04-11 19:35:41 -0400

    Added to get call test predicts

--- model_test.py
+++ model_test.py
@@ -2,8 +2,10 @@ import CaptionDataset
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq
 from nltk.translate.bleu_score import corpus_bleu
 
+from tqdm import tqdm
+
 def get_model():
-    model = AutoModelForSeq2SeqLM.from_pretrained("./checkpoints/checkpt_epoch_1000")
+    model = AutoModelForSeq2SeqLM.from_pretrained("./flan-t5-small-trained")
     tokenizer = AutoTokenizer.from_pretrained("google/flan-t5-small")
 
     return model, tokenizer
@@ -15,9 +17,34 @@ def convert_labels(tensors, tokenizer):
     return tensors
 
 
-def get_bleu_score(inputs:list, labels:list):
-    return corpus_bleu(labels, inputs)
+def get_bleu_score(predicts:list, labels:list):
+    return corpus_bleu(labels, predicts)
 
+def calculate_score(batch_size = 2):
+    model, tokenizer = get_model()
+    dataset = CaptionDataset.get_hf_ds()
+    tokenized_ds = CaptionDataset.tokenize_ds(dataset, tokenizer, deep_copy=True)
+   
+    collator = DataCollatorForSeq2Seq(tokenizer=tokenizer)
+    _, _, test_dataloader = CaptionDataset.get_hf_dataLoaders(tokenized_ds, collator, train_batch=batch_size, val_batch=batch_size, test_batch=batch_size)
+
+
+    labels_lst =[]
+    predicts_lst = []
+    for batch in tqdm(test_dataloader):
+        labels_tensor = convert_labels(batch['labels'], tokenizer)
+
+        labels = tokenizer.batch_decode(labels_tensor, skip_special_tokens=True)
+        
+        outputs = model.generate(**batch)
+        predicts = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+
+        labels_lst.extend(labels)
+        predicts_lst.extend(predicts)
+    
+
+
+        
 
 def test_model(batch_size = 2):
     model, tokenizer = get_model()
@@ -39,4 +66,5 @@ def test_model(batch_size = 2):
     print(tokenizer.batch_decode(convert_labels(batch['labels'], tokenizer), skip_special_tokens=True))
 
 
-test_model(1)
+#test_model(1)
+calculate_score(16)
